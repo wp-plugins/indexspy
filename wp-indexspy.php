87bc936b66a6abca577b03ef1bda260f955c5a40
adbox <adbox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-16 21:11:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/indexspy/trunk@230038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-indexspy.php
+++ wp-indexspy.php
@@ -3,7 +3,7 @@
 	Plugin Name: IndexSpy-WP
 	Plugin URI: http://www.nohatlabs.com/indexspy-wp-released/
 	Description: Check if google indexed your pages/posts. Must have <a href='http://wordpress.org/extend/plugins/google-sitemap-generator/' target=_blank>XML Sitemap Generator Plugin</a> to work with this plugin.
-	Version: 1.5.6
+	Version: 1.5.7
 */
 
 /*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
@@ -171,7 +171,7 @@ function sbis_admin_head(){
 			],
 			height: 'auto',
 			viewrecords: true, 
-			caption:'IndexSpy 1.5.6',
+			caption:'IndexSpy 1.5.7',
 			multiselect: true,
 			rowNum:10000, 
 			//rowList:[1000,2000,3000], 
